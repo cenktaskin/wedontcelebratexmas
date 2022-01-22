2bdaa81dadc0243a10561b44b82bec4540a2b88f
Cenk T <orhuncenktaskin@gmail.com>
2022-01-22 21:29:46 +0100

    cleaned day14

--- day14/day14.py
+++ day14/day14.py
@@ -2,47 +2,49 @@ from collections import Counter
 
 import numpy as np
 
-input_file = "input.txt"
+input_file = "test_input.txt"
 raw_input = np.genfromtxt(input_file, skip_header=2, dtype=str)
-VOCAB = np.array(raw_input[:, 0])
-PAIR_COUNT = len(VOCAB)
-CHAR_VOCAB = np.array(list(Counter("".join(VOCAB)).keys()))
-CHAR_COUNT = len(CHAR_VOCAB)
 
 
-def pair_to_ohe(p):
-    return np.eye(PAIR_COUNT)[VOCAB == p].flatten()
+def ohe(vocab, item):
+    return np.eye(len(vocab))[vocab == item].flatten()
 
 
 def poly_to_ohe(poly):
-    return np.sum([pair_to_ohe(poly[j:j + 2]) for j in range(len(poly) - 1)], axis=0)
+    return np.sum([ohe(pair_vocab, poly[j:j + 2]) for j in range(len(poly) - 1)], axis=0)
 
 
-def pair_to_counts(p):
-    return np.sum([np.eye(CHAR_COUNT)[CHAR_VOCAB == c].flatten() for c in p], axis=0)
+def pair_to_counts(ch_vocab, p):
+    return np.sum([ohe(ch_vocab, c) for c in p], axis=0)
 
 
 def take_step(arr):
     new_arr = np.zeros_like(arr)
-    avaliable_pairs = np.diag(arr)[arr > 0]
-    for p in avaliable_pairs:
+    available_pairs = np.diag(arr)[arr > 0]
+    for p in available_pairs:
         multiplier = np.sum(p)
         new_arr += multiplier * reaction_table[tuple(p / multiplier)]
     return new_arr
 
 
-bins = np.array([pair_to_counts(p) for p in VOCAB])
+def poly_to_counts(vocab, pol, pol0):
+    ch_vocab = np.array(list(Counter("".join(vocab)).keys()))
+    bins = np.array([pair_to_counts(ch_vocab, p) for p in vocab])
+    first_and_last = pair_to_counts(ch_vocab, pol0[0] + pol0[-1])
+    return (bins.T @ pol + first_and_last) / 2
 
-reaction_table = {tuple(pair_to_ohe(a)): poly_to_ohe(a[0] + b + a[1]) for a, b in
+
+pair_vocab = np.array(raw_input[:, 0])
+
+reaction_table = {tuple(poly_to_ohe(a)): poly_to_ohe(a[0] + b + a[1]) for a, b in
                   zip(raw_input[:, 0], raw_input[:, -1])}
 
 starting_polymer = open(input_file, "r").readline().rstrip()
-first_and_last = pair_to_counts(starting_polymer[0] + starting_polymer[-1])
 
 p = poly_to_ohe(starting_polymer)
 for i in range(40):
     p = take_step(p)
 
-hist = (bins.T @ p + first_and_last) / 2
+hist = poly_to_counts(pair_vocab, p, starting_polymer)
 print(hist)
 print(int(hist.max() - hist.min()))
